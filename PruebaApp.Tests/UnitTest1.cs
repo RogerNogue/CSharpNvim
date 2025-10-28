09034bab79b7b8b867d593574ffa80e9df9f1bf9
rogernogue <rogernogueb@gmail.com>
2025-10-28 18:50:10 +0100

    chore: added all files

---
+++ PruebaApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using DotnetTestApp;
+
+namespace DotnetTestApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestAdd()
+    {
+        Assert.Equal(5, Program.Add(2, 3));
+    }
+}
+
---
+++ PruebaApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DotnetTestApp;
+
+public class Program
+{
+    public static int Add(int a, int b) => a + b;
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"2 + 3 = {Add(2, 3)}");
+    }
+}
